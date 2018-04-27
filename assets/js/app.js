1aa00b950672bfa232a7994f5f71ffc7d7235a5a
Justas Kuizinas <4token@gmail.com>
2018-04-27 18:01:21 +0300

    'updates'

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,123 @@
         initBussinesSwiper();
         initYT();
         initOffCanvas();
-        initParticles();
+        initOrcParticles();
+        initPaladinParticles();
         initAccodron();
+        initRangeSlider()
+        initTextSlider();
+        initSticky();
     });
+
+    function initSticky() {
+        var $menu = $('.menu');
+
+        detect();
+        $(document).scroll(function () {
+            detect();
+        });
+
+        function detect() {
+            if ($(document).scrollTop() > 100) {
+                $('.menu').addClass('is-sticky');
+            } else {
+                $('.menu').removeClass('is-sticky');
+            }
+        }
+    }
+
+    function initTextSlider() {
+        var swiper = new Swiper('.text-slider .swiper-container', {
+            // Optional parameters
+            loop: true,
+
+            // If we need pagination
+            pagination: {
+                el: '.text-slider .swiper-pagination',
+                type: 'bullets',
+                clickable: true
+
+            },
+            autoplay: {
+                delay: 5000
+            },
+        });
+    }
+
+    function initRangeSlider() {
+        var $transactionCut = $('.calc .transaction-cut');
+        var $cosmetics = $('.calc .cosmetics');
+        var $ladderEntrance = $('.calc .ladder-entrance');
+        var $ransom = $('.calc .ransom');
+        var $horizont = $('.horizont');
+
+        var vals = {
+            2018: {
+                'transactionCut': 1,
+                'cosmetics': 2,
+                'ladderEntrance': 3,
+                'ransom': 4
+            },
+            2019: {
+                'transactionCut': 5,
+                'cosmetics': 6,
+                'ladderEntrance': 7,
+                'ransom': 8
+            },
+            2020: {
+                'transactionCut': 9,
+                'cosmetics': 10,
+                'ladderEntrance': 11,
+                'ransom': 12
+            },
+            2021: {
+                'transactionCut': 13,
+                'cosmetics': 14,
+                'ladderEntrance': 15,
+                'ransom': 16
+            },
+            2022: {
+                'transactionCut': 17,
+                'cosmetics': 18,
+                'ladderEntrance': 19,
+                'ransom': 20
+            }
+        }
+
+
+        changeProjection(Object.keys(vals)[0]);
+
+        $('.calc input[type="range"]').rangeslider({
+            polyfill: false,
+
+            // Default CSS classes
+            rangeClass: 'rangeslider',
+            disabledClass: 'rangeslider--disabled',
+            horizontalClass: 'rangeslider--horizontal',
+            verticalClass: 'rangeslider--vertical',
+            fillClass: 'rangeslider__fill',
+            handleClass: 'rangeslider__handle',
+
+            onInit: function () {
+            },
+            onSlide: f.debounce(function (position, value) {
+                changeProjection(value);
+            }, 15),
+            onSlideEnd: function (position, value) {
+
+            }
+        });
+
+        function changeProjection(year) {
+            var opacity = (1 / Object.keys(vals).length) * (Object.keys(vals).indexOf(String(year)) + 1);
+            $horizont.css('opacity', opacity);
+
+            $transactionCut.html(vals[year].transactionCut + ' €');
+            $cosmetics.html(vals[year].cosmetics + ' €');
+            $ladderEntrance.html(vals[year].ladderEntrance + ' €');
+            $ransom.html(vals[year].ransom + ' €');
+        }
+    }
 
     function initAccodron() {
         var $accordion = $('#accordion');
@@ -23,140 +137,250 @@
 
     }
 
-    function initParticles() {
-        var app = new PIXI.Application();
-        console.log(app);
-        //$('.orc').append(app.view);
+    function initOrcParticles() {
+        /*
+       var cv_wrap  = document.getElementById("cv_wrap");
+       var cvWidth  = cv_wrap.offsetWidth | 0;
+       var cvHeight = cv_wrap.offsetHeight | 0;
+       */
+
+        var $section = $('.orc');
+
+        var addWidth = 500;
+        // var cvWidth = $section.outerWidth() + addWidth;
+        // var cvHeight = $section.outerHeight();
+        var cvWidth = 1000;
+        var cvHeight = 1000;
+
+        /*
+        console.dir(cv_wrap);
+        console.log('cvWidth：'+cvWidth);
+        console.log('cvHeight：'+cvHeight);
+        */
+
+        var renderer = PIXI.autoDetectRenderer(cvWidth, cvHeight, {transparent: true});
+
+        //cv_wrap.appendChild(renderer.view);
+        $section.prepend(renderer.view);
+
+        // create the root of the scene graph
+        var container = new PIXI.Container();
+
 
         var emitter = new PIXI.particles.Emitter(
-            // The PIXI.Container to put the emitter in
-            // if using blend modes, it's important to put this
-            // on top of a bitmap, and not use the root stage Container
-            app.container,
-
-            [PIXI.Texture.fromImage('image.jpg')],
+            container,
+            [
+                PIXI.Texture.fromImage('assets/img/p1.png'),
+                PIXI.Texture.fromImage('assets/img/p2.png'),
+                PIXI.Texture.fromImage('assets/img/p3.png')
+            ],
 
             {
-                alpha: {
-                    list: [
-                        {
-                            value: 0.8,
-                            time: 0
-                        },
-                        {
-                            value: 0.1,
-                            time: 1
-                        }
-                    ],
-                    isStepped: false
+                "alpha": {
+                    "start": 1,
+                    "end": 0
                 },
-                scale: {
-                    list: [
-                        {
-                            value: 1,
-                            time: 0
-                        },
-                        {
-                            value: 0.3,
-                            time: 1
-                        }
-                    ],
-                    isStepped: false
+                "scale": {
+                    "start": 0.7,
+                    "end": 1,
+                    "minimumScaleMultiplier": 1
                 },
-                color: {
-                    list: [
-                        {
-                            value: "fb1010",
-                            time: 0
-                        },
-                        {
-                            value: "f5b830",
-                            time: 1
-                        }
-                    ],
-                    isStepped: false
+                "color": {
+                    "start": "#ffffff",
+                    "end": "#9ff3ff"
                 },
-                speed: {
-                    list: [
-                        {
-                            value: 200,
-                            time: 0
-                        },
-                        {
-                            value: 100,
-                            time: 1
-                        }
-                    ],
-                    isStepped: false
+                "speed": {
+                    "start": 5,
+                    "end": 8,
+                    "minimumSpeedMultiplier": 5
                 },
-                startRotation: {
-                    min: 0,
-                    max: 360
+                "acceleration": {
+                    "x": -15,
+                    "y": 0
                 },
-                rotationSpeed: {
-                    min: 0,
-                    max: 0
+                "maxSpeed": 0,
+                "startRotation": {
+                    "min": 0,
+                    "max": 360
                 },
-                lifetime: {
-                    min: 0.5,
-                    max: 0.5
+                "noRotation": false,
+                "rotationSpeed": {
+                    "min": 5,
+                    "max": 100
                 },
-                frequency: 0.008,
-                spawnChance: 1,
-                particlesPerWave: 1,
-                emitterLifetime: 0.31,
-                maxParticles: 1000,
-                pos: {
-                    x: 0,
-                    y: 0
+                "lifetime": {
+                    "min": 2,
+                    "max": 8
                 },
-                addAtBack: false,
-                spawnType: "circle",
-                spawnCircle: {
-                    x: 0,
-                    y: 0,
-                    r: 10
+                "blendMode": "normal",
+                "frequency": 0.003,
+                "emitterLifetime": -1,
+                "maxParticles": 700,
+                "pos": {
+                    "x": 0,
+                    "y": 0
+                },
+                "addAtBack": false,
+                "spawnType": "circle",
+                "spawnCircle": {
+                    "x": 0,
+                    "y": 0,
+                    "r": 300
                 }
             }
         );
 
-        var elapsed = Date.now();
+        /*
+        console.log('cvWidth / 2 = '+(cvWidth / 2));
+        console.log('cvHeight / 2 = '+(cvHeight / 2));
+        */
 
+        emitter.updateOwnerPos((cvWidth / 2), (cvHeight / 2));
+        // emitter.updateOwnerPos(800, 380);
+        emitter.emit = true;
+
+        var elapsed = Date.now();
         var update = function () {
-            requestAnimationFrame(update);
+
             var now = Date.now();
             emitter.update((now - elapsed) * 0.001);
             elapsed = now;
 
-            // Should re-render the PIXI Stage
-            app.renderer.render(app.stage);
-        };
+            renderer.render(container);
 
-        emitter.emit = true;
+            requestAnimationFrame(update);
+        };
         update();
 
-        // PIXI.loader.add('bunny', 'bunny.png').load((loader, resources) => {
-        //         // This creates a texture from a 'bunny.png' image
-        //         const bunny = new PIXI.Sprite(resources.bunny.texture);
-        //
-        //     // Setup the position of the bunny
-        //     bunny.x = app.renderer.width / 2;
-        //     bunny.y = app.renderer.height / 2;
-        //
-        //     // Rotate around the center
-        //     bunny.anchor.x = 0.5;
-        //     bunny.anchor.y = 0.5;
-        //
-        //     // Add the bunny to the scene we are building
-        //     app.stage.addChild(bunny);
-        //
-        //     // Listen for frame updates
-        //     app.ticker.add(() => {
-        //         // each frame we spin the bunny around a bit
-        //         bunny.rotation += 0.01;
-        // });
-        // });
+        window.onresize = function (event) {
+            //renderer.resize($section.outerWidth() + addWidth, $section.outerHeight());
+        };
+        window.onresize();
+
     }
+
+
+    function initPaladinParticles() {
+        /*
+       var cv_wrap  = document.getElementById("cv_wrap");
+       var cvWidth  = cv_wrap.offsetWidth | 0;
+       var cvHeight = cv_wrap.offsetHeight | 0;
+       */
+
+        var $section = $('.paladin');
+
+        var addWidth = 500;
+        // var cvWidth = $section.outerWidth() + addWidth;
+        // var cvHeight = $section.outerHeight();
+        var cvWidth = 1000;
+        var cvHeight = 1000;
+
+        /*
+        console.dir(cv_wrap);
+        console.log('cvWidth：'+cvWidth);
+        console.log('cvHeight：'+cvHeight);
+        */
+
+        var renderer = PIXI.autoDetectRenderer(cvWidth, cvHeight, {transparent: true});
+
+        //cv_wrap.appendChild(renderer.view);
+        $section.prepend(renderer.view);
+
+        // create the root of the scene graph
+        var container = new PIXI.Container();
+
+
+        var emitter = new PIXI.particles.Emitter(
+            container,
+            [
+                PIXI.Texture.fromImage('assets/img/smokeparticle.png'),
+            ],
+
+            {
+                "alpha": {
+                    "start": 0.5,
+                    "end": 0.1
+                },
+                "scale": {
+                    "start": 0.1,
+                    "end": 1.5,
+                    "minimumScaleMultiplier": 1.1
+                },
+                "color": {
+                    "start": "#5a4d59",
+                    "end": "#5a4d59"
+                },
+                "speed": {
+                    "start": 50,
+                    "end": 100,
+                    "minimumSpeedMultiplier": 1
+                },
+                "acceleration": {
+                    "x": 0,
+                    "y": 0
+                },
+                "maxSpeed": 0,
+                "startRotation": {
+                    "min": 200,
+                    "max": 290
+                },
+                "noRotation": false,
+                "rotationSpeed": {
+                    "min": 0,
+                    "max": 100
+                },
+                "lifetime": {
+                    "min": 1,
+                    "max": 10
+                },
+                "blendMode": "normal",
+                "frequency": 0.001,
+                "emitterLifetime": -1,
+                "maxParticles": 800,
+                "pos": {
+                    "x": -250,
+                    "y": -25
+                },
+                "addAtBack": true,
+                "spawnType": "rect",
+                "spawnRect": {
+                    "x": 0,
+                    "y": 0,
+                    "w": 500,
+                    "h": 50
+                }
+            }
+        );
+
+        /*
+        console.log('cvWidth / 2 = '+(cvWidth / 2));
+        console.log('cvHeight / 2 = '+(cvHeight / 2));
+        */
+
+        emitter.updateOwnerPos((cvWidth / 2), cvHeight);
+        // emitter.updateOwnerPos(800, 380);
+        emitter.emit = true;
+
+        var elapsed = Date.now();
+        var update = function () {
+
+            var now = Date.now();
+            emitter.update((now - elapsed) * 0.001);
+            elapsed = now;
+
+            renderer.render(container);
+
+            requestAnimationFrame(update);
+        };
+        update();
+
+        window.onresize = function (event) {
+            //renderer.resize($section.outerWidth() + addWidth, $section.outerHeight());
+        };
+        window.onresize();
+
+    }
+
 
     function initBussinesSwiper() {
         var swiper;
@@ -194,27 +418,18 @@
     }
 
     function initYT() {
-        var trigger = $("body").find('[data-toggle="modal"]');
-        var theModal,
-            videoSRC,
-            videoSRCauto;
+        var theModal = '#videoModal';
+        var videoSRC = 'https://www.youtube.com/embed/f4uO7miigQw' + "?autoplay=1";
 
-        trigger.click(function () {
-            var that = this;
-            setTimeout(function () {
-                theModal = $(that).data("target");
-                videoSRC = $(that).attr("data-theVideo");
-                videoSRCauto = videoSRC + "?autoplay=1";
-                $(theModal + ' iframe').attr('src', videoSRCauto);
-                $(theModal + ' button.close').click(function () {
-                    $(theModal + ' iframe').attr('src', videoSRC);
-                });
-            }, 300);
-
-        });
 
         $('#videoModal').on('hidden.bs.modal', function (e) {
             $(theModal + ' iframe').attr('src', '');
+        });
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $(theModal + ' iframe').attr('src', videoSRC);
+            $(theModal + ' button.close').click(function () {
+                $(theModal + ' iframe').attr('src', '');
+            });
         });
     }
 
