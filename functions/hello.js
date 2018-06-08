ea4bd1fe034555d9dbfdff1470cf8e9bd9c3c298
Justas Kuizinas <4token@gmail.com>
2018-06-08 14:40:00 +0300

    test

--- functions/hello.js
+++ functions/hello.js
@@ -1,9 +1,9 @@
-var subscribersCount = 2556;
 
 exports.handler = function(event, context, callback) {
+    var subscribersCount = 2556; 
     subscribersCount++;
     callback(null, {
         statusCode: 200,
-        body: subscribersCount 
+        body: subscribersCount
     });
 }
