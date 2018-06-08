15264bc9c14cfd220c18053c7361bda241a23c4e
Justas Kuizinas <4token@gmail.com>
2018-06-08 15:47:39 +0300

    test

--- functions/subscriberscount.js
+++ functions/subscriberscount.js
@@ -2,12 +2,12 @@ exports.handler = function (event, context, callback) {
 
     if (event.httpMethod == 'POST') {
         process.env.subscribersCount = parseInt(process.env.subscribersCount) + 1;
-        var subscriberscount = process.env.subscribersCount; 
+        var subscriberscount = process.env.subscribersCount;
         callback(null, {
             statusCode: 200,
             body: "" + subscriberscount
         });
-    }
+    }   
     if (event.httpMethod == 'GET') {
         var subscriberscount = parseInt(process.env.subscribersCount);
         callback(null, {
