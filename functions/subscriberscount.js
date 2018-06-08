b43bcb6bb9be35144f84b37d4b52051abb7100f9
Justas Kuizinas <4token@gmail.com>
2018-06-08 14:58:46 +0300

    up

---
+++ functions/subscriberscount.js
@@ -0,0 +1,20 @@
+var subscriberscount = 2556;
+ 
+exports.handler = function(event, context, callback) {
+
+    subscriberscount++;
+    if(event.httpMethod=='POST'){
+        subscriberscount++;
+        callback(null, {
+            statusCode: 200,
+            body: ""+subscriberscount
+        });
+    }   
+    if(event.httpMethod=='GET'){
+        callback(null, {
+            statusCode: 200,
+            body: ""+subscriberscount
+        });
+    }
+
+}
